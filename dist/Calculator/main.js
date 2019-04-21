f820e47afd6a9ed1a9a610b72caba2f77456f8c1
Eric Truter <eric.truter@live.com>
2019-04-21 17:13:36 +0200

    AB#12
    added junit unit testing to azure devops

--- dist/Calculator/main.js
+++ dist/Calculator/main.js
@@ -217,7 +217,7 @@ _angular_platform_browser__WEBPACK_IMPORTED_MODULE_3__["platformBrowser"]().boot
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Dev\Github\calculator\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! D:\dev\Github\calculator\src\main.ts */"./src/main.ts");
 
 
 /***/ })
